f10e203263dbce503027c0e1fbf0a315396b22a9
Rushi Solanki <rushisolanki6@gmail.com>
2022-09-01 16:08:47 +0530

    added branch b line

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 
-export default function HelloWorld(){
-  return(
-    <View style = {styles.container}>
-      <Text style = {styles.text}>Hello World!!!!</Text>
+export default function HelloWorld() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World!!!!</Text>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 
-  text:{
-    fontSize: 30
-  }
-})
+  text: {
+    fontSize: 30,
+  },
+});
+
+//branch B line
